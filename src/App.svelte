1f9d3274472076efef6752be5c05b71d593195f0
andrey.shedko@gmail.com <andrey.shedko@gmail.com>
2024-04-29 22:40:40 +0200

    Basic navigation added

--- src/App.svelte
+++ src/App.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
+    import "./app.scss";
+    import Navigation from "../src/lib/components/Navigation.svelte";
 </script>
 
-<main>
-</main>
+<Navigation />
---
+++ src/lib/components/Navigation.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  import {Route} from 'tinro'; 
+    import Cabinet from './Cabinet.svelte';
+    import Home from './Home.svelte';
+</script>
+
+<!-- svelte-ignore a11y-missing-attribute -->
+<nav class="navbar" role="navigation" aria-label="main navigation">
+  <div class="navbar-brand">
+    <a class="navbar-item" href="https://automato.store">
+      <img src="./roboto.svg" />
+    </a>
+
+    <a
+      role="button"
+      class="navbar-burger"
+      aria-label="menu"
+      aria-expanded="false"
+      data-target="navbarBasicExample"
+    >
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+      <span aria-hidden="true"></span>
+    </a>
+  </div>
+
+  <div id="navbarBasicExample" class="navbar-menu">
+    <div class="navbar-start">
+      <a class="navbar-item" href="/"> Home </a>
+
+      <a class="navbar-item"> Documentation </a>
+
+      <div class="navbar-item has-dropdown is-hoverable">
+        <a class="navbar-link"> More </a>
+
+        <div class="navbar-dropdown">
+          <a class="navbar-item"> About </a>
+          <a class="navbar-item is-selected"> Jobs </a>
+          <a class="navbar-item"> Contact </a>
+          <hr class="navbar-divider" />
+          <a class="navbar-item"> Report an issue </a>
+        </div>
+      </div>
+    </div>
+
+    <div class="navbar-end">
+      <div class="navbar-item">
+        <div class="buttons">
+          <a class="button is-primary">
+            <strong>Sign up</strong>
+          </a>
+          <a class="button is-light" href="/cabinet"> Log in </a>
+        </div>
+      </div>
+    </div>
+  </div>
+</nav>
+
+<Route path="/"><Home /></Route>
+<Route path="/cabinet"><Cabinet /></Route>
