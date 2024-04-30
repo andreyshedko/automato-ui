423c846e7bbcd8e12dc65eb449d35b6edf41cdd8
andrey.shedko@gmail.com <andrey.shedko@gmail.com>
2024-04-30 23:08:41 +0200

    Add Auth0

--- src/lib/components/Login.svelte
+++ src/lib/components/Login.svelte
@@ -1,8 +1,33 @@
-<script lang="ts">
-	export let data;
+<script>
+  import { onMount } from "svelte";
+  import auth from "../stores/auth.service";
+  import { isAuthenticated, user } from "../stores/auth";
+
+  let auth0Client;
+  let newTask;
+
+  onMount(async () => {
+    auth0Client = await auth.createClient();
+
+    isAuthenticated.set(await auth0Client.isAuthenticated());
+    user.set(await auth0Client.getUser());
+  });
+
+  function login() {
+    auth.loginWithPopup(auth0Client);
+  }
+
+  function logout() {
+    auth.logout(auth0Client);
+  }
 </script>
 
-<h3>Login</h3>
-<form method="POST" action="?/{data?.user ? 'signOut' : 'signIn'}">
-  <button type="submit">Sign {data?.user ? 'out' : 'in'}</button>
-</form>
+{#if $isAuthenticated}
+  <li class="nav-item">
+    <a class="nav-link" href="/#" on:click={logout}>Log Out</a>
+  </li>
+{:else}
+  <li class="nav-item">
+    <a class="nav-link" href="/#" on:click={login}>Log In</a>
+  </li>
+{/if}
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
-import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { svelte } from '@sveltejs/vite-plugin-svelte';
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('C:\\Users\\andre\\AppData\\Local\\mkcert\\127.0.0.1+1-key.pem'),
+      cert: fs.readFileSync('C:/Users/andre/AppData/Local/mkcert//127.0.0.1+1.pem')
+    },
+    proxy: {}
+  },
   plugins: [svelte()],
+  optimizeDeps: { exclude: ["svelte-navigator"] },
 })
