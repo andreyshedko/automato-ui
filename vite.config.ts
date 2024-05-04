03c6b096fb5241fd4b7423db36d2ace79f6640ce
andrey.shedko@gmail.com <andrey.shedko@gmail.com>
2024-05-04 13:38:27 +0200

    Translations added

--- vite.config.ts
+++ vite.config.ts
@@ -2,14 +2,13 @@ import { defineConfig } from 'vite';
 import { sveltekit } from '@sveltejs/kit/vite';
 import fs from 'fs';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // server: {
-  //   https: {
-  //     key: fs.readFileSync('C:\\Users\\andre\\AppData\\Local\\mkcert\\127.0.0.1+1-key.pem'),
-  //     cert: fs.readFileSync('C:\\Users\\andre\\AppData\\Local\\mkcert\\127.0.0.1+1.pem')
-  //   },
-  //   proxy: {}
-  // },
+  server: {
+    https: {
+      key: fs.readFileSync('C:\\Users\\andre\\AppData\\Local\\mkcert\\127.0.0.1+1-key.pem'),
+      cert: fs.readFileSync('C:\\Users\\andre\\AppData\\Local\\mkcert\\127.0.0.1+1.pem')
+    },
+    proxy: {}
+  },
   plugins: [sveltekit()],
 })
